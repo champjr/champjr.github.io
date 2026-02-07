43a3c6e018d372fe53c14c8e5a1ce8d42f7d7400
Champ <champjrlim@gmail.com>
2026-02-07 11:19:32 -0600

    Deploy to GitHub Pages via Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://github.com/champjr/champ-blog',
+  // GitHub Pages (project site)
+  site: 'https://champjr.github.io',
+  base: '/champ-blog',
 });
